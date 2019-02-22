a126f23b04e33c2146e43632810f4d42db1af531
Xu Peng <xupeng@tictalk.com>
2019-02-23 00:11:56 +0800

    refactor(cmake): add third package

--- compiler/ADT/main.cpp
+++ compiler/ADT/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <boost/bind.hpp>
 
 using namespace std;
 
+void aaa(int a) {
+    std::cout << a << std::endl;
+}
+
 int main(int argc, char** argv) {
     cout << "hello adt lab" << endl;
+
+    boost::bind(&aaa, 10)();
+
     return 0;
 }
