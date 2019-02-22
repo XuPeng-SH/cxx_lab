9153f84a099c63532de8826210e1e93aa7da3ad7
Xu Peng <xupeng@tictalk.com>
2019-02-22 16:45:25 +0800

    feat(subproj): add cmake_examples subproject

---
+++ cmake_examples/A-Sample/src/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.hpp"
+#include <iostream>
+
+void say_hello() {
+    std::cout << "say_hello from ASample" << std::endl;
+}
---
+++ cmake_examples/A-Sample/src/main.cpp
@@ -0,0 +1,6 @@
+#include "hello.hpp"
+
+int main(int argc, char** argv) {
+    say_hello();
+    return 0;
+}
