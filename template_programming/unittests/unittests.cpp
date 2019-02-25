fd42cb4fedb92b539f6a3306863dc523b8489311
Xu Peng <xupeng@tictalk.com>
2019-02-25 18:37:50 +0800

    test(gtest): add more test

--- template_programming/unittests/unittests.cpp
+++ template_programming/unittests/unittests.cpp
@@ -3,6 +3,9 @@
 
 #include <gtest/gtest.h>
 
+
+DEFINE_HAS_CLASS_TYPE(second_argument_type);
+
 class MyUnitTest : public ::testing::Test {
 
 };
@@ -15,3 +18,25 @@ TEST_F(MyUnitTest, is_pointer) {
     is_a_pointer = is_pointer<ConstCharT>::value;
     EXPECT_EQ(is_a_pointer, false);
 };
+
+TEST_F(MyUnitTest, MyBinder2nd) {
+    MyBinder2nd<std::less<int>, int> binder(std::less<int>(), 10);
+    bool has = HAS_CLASS_TYPE(std::less<int>, second_argument_type);
+    EXPECT_EQ(has, true);
+
+    typedef MyBinder2nd<std::less<int>,int> MyBinder2ndType;
+    has = HAS_CLASS_TYPE(MyBinder2ndType, second_argument_type);
+    EXPECT_EQ(has, false);
+
+    has = FunctionChecker<void ()>::has_first_argument;
+    EXPECT_EQ(has, false);
+
+    has = FunctionChecker<std::less<int>>::has_first_argument;
+    EXPECT_EQ(has, true);
+
+    has = FunctionChecker<std::less<int>>::has_second_argument;
+    EXPECT_EQ(has, true);
+
+    has = FunctionChecker<MyBinder2nd<std::less<int>, int>>::has_second_argument;
+    EXPECT_EQ(has, false);
+}
