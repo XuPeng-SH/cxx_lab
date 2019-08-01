e02974b7c144ec0e8f562a634f9819c313d48ad9
peng.xu <peng.xu@zilliz.com>
2019-08-01 18:46:58 +0800

    update for faiss lab

--- faiss_lab/main.cpp
+++ faiss_lab/main.cpp
@@ -9,7 +9,8 @@
 
 using namespace std;
 
-void demo() {
+void flat_test() {
+    int times = 5;
     INIT_TIMER;
     START_TIMER;
     int d = 512;                            // dimension
@@ -25,32 +26,24 @@ void demo() {
     flat_l2.add(data.nb, data.xb);
     STOP_TIMER("ADD CPU XB: ");
 
-    cout << "ntotal=" << flat_l2.ntotal << endl;
-    int times = 5;
     search_index_test(&flat_l2, "CpuSearchTest", 20, 10, data.nb, data.xb, times);
 
-    faiss::Index* cpu_index = &flat_l2;
     faiss::gpu::StandardGpuResources gpu_res;
 
+    faiss::Index* cpu_index = &flat_l2;
     cpu_to_gpu_test(&gpu_res, cpu_index, "CpuToGpuTEST", 5);
 
     long start_nb = data.nb;
     long step_nb = -100000;
     long end_nb = 100000;
-
-
     gpu_add_vectors_test(&gpu_res, "GpuAddVectorsTest", 3, start_nb, end_nb, step_nb, data.xb, data.d);
 
     faiss::gpu::GpuIndexFlatL2 gpu_index_flat(&gpu_res, d);
     gpu_index_flat.add(data.nb, data.xb);
     search_index_test(&gpu_index_flat, "GpuSearchTest", 20, 10, data.nb, data.xb, times);
-
-    /* delete [] xb; */
-    /* delete [] xq; */
 }
 
 int main() {
     faiss::distance_compute_blas_threshold = 800;
-    cout << "Hello" << endl;
-    demo();
+    flat_test();
 }
