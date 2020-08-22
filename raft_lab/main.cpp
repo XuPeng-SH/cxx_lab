c3a934a2bd7dc2a622207e8b363d6ba81f65111a
XuPeng-SH <xupeng3112@163.com>
2020-08-22 19:51:54 +0800

    update

---
+++ raft_lab/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <sstream>
+#include <fstream>
+#include <assert.h>
+#include <atomic>
+#include <folly/executors/ThreadedExecutor.h>
+#include <folly/futures/Future.h>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    return 0;
+}
