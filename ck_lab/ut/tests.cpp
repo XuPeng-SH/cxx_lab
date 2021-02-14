823d8470d9b09cf05ff7e0f69c14431f8b324543
XuPeng-SH <xupeng3112@163.com>
2021-02-14 23:43:52 +0800

    add MergePipe test

--- ck_lab/ut/tests.cpp
+++ ck_lab/ut/tests.cpp
@@ -183,7 +183,7 @@ TEST_F(MyUT, Pipe1) {
     /* pipe.AddTransform(sink); */
 }
 
-TEST_F(MyUT, Pipe2) {
+TEST_F(MyUT, MutltiSourcePipe) {
     Pipe pipe;
     ASSERT_EQ(pipe.OutputPortSize(), 0);
     ASSERT_TRUE(pipe.Empty());
@@ -226,4 +226,71 @@ TEST_F(MyUT, Pipe2) {
     ASSERT_EQ(pipe.NumOfProcessors(), 5);
     ASSERT_EQ(pipe.OutputPortSize(), t3->OutputPortSize());
     /* pipe.AddTransform(sink); */
+}
+
+PipePtr
+CreatePipe() {
+    int chain_size = RandomInt(0, 4);
+
+    vector<size_t> degrees = {0, 1};
+    for (auto i = 0; i < chain_size; ++i) {
+        degrees.emplace_back(RandomInt(1, 4));
+    }
+
+    Processors processors;
+    size_t input_size = degrees[0];
+    size_t output_size = 0;
+
+    for (auto i = 1; i < degrees.size(); ++i) {
+        output_size = degrees[i];
+        processors.push_back(CreateProcessor(input_size, output_size));
+        input_size = output_size;
+    }
+
+    auto pipe = std::make_shared<Pipe>();
+    auto first = true;
+    for (auto processor : processors) {
+        if (first) {
+            first = false;
+            pipe->AddSource(processor);
+        } else {
+            pipe->AddTransform(processor);
+        }
+    }
+    return pipe;
+}
+
+Pipes
+CreatePipes(size_t n) {
+    Pipes pipes;
+    for (auto i = 0; i < n; ++i) {
+        auto pipe = CreatePipe();
+        pipes.push_back(pipe);
+        /* std::cout << pipe->ToString() << std::endl; */
+    }
+    return std::move(pipes);
+}
+
+TEST_F(MyUT, MergePipes) {
+    auto pipes = CreatePipes(RandomInt(2, 5));
+    size_t pre_max = 0;
+    size_t pre_num_processors = 0;
+    size_t pre_output_size = 0;
+    for (auto& p : pipes) {
+        pre_max += p->MaxParallelStreams();
+        pre_output_size += p->OutputPortSize();
+        pre_num_processors += p->NumOfProcessors();
+    }
+    auto pipe = Pipe::MergePipes(pipes);
+    cout << pipe->ToString() << endl;
+    ASSERT_EQ(pre_max, pipe->MaxParallelStreams());
+    ASSERT_EQ(pre_output_size, pipe->OutputPortSize());
+    ASSERT_EQ(pre_num_processors, pipe->NumOfProcessors());
+
+    auto output = RandomInt(1,4);
+    auto t1 = CreateProcessor(pipe->OutputPortSize(), output);
+    pipe->AddTransform(t1);
+    ASSERT_EQ(pipe->NumOfProcessors(), pre_num_processors + 1);
+    ASSERT_EQ(pipe->OutputPortSize(), t1->OutputPortSize());
+    cout << pipe->ToString() << endl;
 }
