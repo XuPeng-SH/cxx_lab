7c602fb1489fceb7f651473c684b5b4ed6d2c560
XuPeng-SH <xupeng3112@163.com>
2021-02-25 22:56:02 +0800

    add HierarchyGuard

--- ck_lab/main.cpp
+++ ck_lab/main.cpp
@@ -5,6 +5,7 @@
 
 #include "Port.h"
 #include "Graph.h"
+#include "HierarchyGuard.h"
 
 using namespace std;
 using namespace MyDB;
@@ -12,6 +13,14 @@ using namespace MyDB;
 
 int main(int argc, char** argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    std::unique_lock<std::mutex> guards_lock;
+    StrIDGuard::HighMutexPtr high_mutex;
+    StrIDGuard::RelationPtr rel = std::make_shared<StrIDGuard::Relation>();
+
+    StrIDGuard guard(high_mutex, "", rel, "");
+    /* StrIDGuard guard(high_mutex, "", rel, "", std::move(guards_lock)); */
+    cout << guard.GetEntryCount("") << endl;
 
     /* shared_ptr<int> a = nullptr; */
     /* shared_ptr<int> b = std::make_shared<int>(2); */
